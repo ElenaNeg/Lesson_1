2174e39a12aa6ca067f65111a7d7903ba5c748dd
Elena Negerish <elenkaNeg@yandex.ru>
2022-08-08 17:32:11 +0300

    Ex004 Div

---
+++ Ex004_Div/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+int numberA = 10;
+int numberB = 5;
+Console.WriteLine (numberA / numberB);
+*/
+/*
+int numberA = 12;
+int numberB = 5;
+Console.WriteLine ( numberA / numberB);
+*/
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine ( numberA / numberB);
+

f9a102cb8c075f14c50f7d62f8287fd37dbbe4a2
Elena Negerish <elenkaNeg@yandex.ru>
2022-08-08 22:56:46 +0300

    Ex006 Condition if else

---
+++ Ex006_Condition_IfElse/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine( );
+
+if (username == "Маша")
+{
+    Console.WriteLine ("Ура, это же МАША!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
+
+*/
+/*
+Если написать имя с маленькой буквы, то по особенному любимчика не будут приветствовать. Поэтому запишем по новому 
+*/ 
+
+Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine( );
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура, это же МАША!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}

624c2911bb3edcd52d54becfd5f2a583e44e39e1
Elena Negerish <elenkaNeg@yandex.ru>
2022-08-08 17:15:52 +0300

    Ex003 sum

---
+++ Ex003_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+int numberA = 3;
+int numberB = 5;
+Console.WriteLine ( numberA + numberB);
+*/
+
+int numberA = 4;
+int numberB = 9;
+int result = numberA + numberB;
+Console.WriteLine ( result );
